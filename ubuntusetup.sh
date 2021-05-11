12f2889a1d4ed3ee4aaca28cd2479d0c47e26dec
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-05-11 23:30:48 +0300

    Update ubuntusetup.sh

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -1,14 +1,17 @@
 sudo sh -c "sed -i '/deb-src/s/^# //' /etc/apt/sources.list" # enable Source Repos for build-dep)
 
-sudo apt update
-sudo apt upgrade -y
+echo 'Updating'
+sudo apt -qq update
+
+echo 'Upgrading'
+sudo apt -qq upgrade -y
 
 echo 'Installing Git'
-sudo apt install -y git
+sudo apt -qq install -y git
 git config --global core.editor vim
 
 echo 'Installing Vim'
-sudo apt install -y vim vim-gtk
+sudo apt -qq install -y vim vim-gtk
 
 echo 'Setting up vim'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -26,7 +29,7 @@ echo 'Installing opera'
 sudo snap install opera
 
 echo 'Installing rbenv'
-sudo apt install -y rbenv
+sudo apt -qq install -y rbenv
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
@@ -41,13 +44,13 @@ echo 'Installing rails'
 gem install rails
 
 echo 'Installing Imagemagick'
-sudo apt-get install -y build-essential
-sudo apt-get build-dep imagemagick -y
+sudo apt -qq install -y build-essential
+sudo apt -qq build-dep imagemagick -y
 cd ~/Downloads
 wget https://download.imagemagick.org/ImageMagick/download/ImageMagick.tar.gz
 tar -zxvf ImageMagick.tar.gz
 cd ImageMagick-*
-sudo apt-get install -y libltdl-dev
+sudo apt -qq install -y libltdl-dev
 ./configure --with-modules
 make
 sudo make install
@@ -56,7 +59,7 @@ cd
 
 # todo: compile latest version from source
 echo 'Installing redis'
-sudo apt install -y redis-server
+sudo apt -qq install -y redis-server
 
 echo 'Installing chromedriver'
 cd ~/Downloads
@@ -68,8 +71,8 @@ sudo cp chromedriver /bin/
 echo 'postgresql'
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install postgresql postgresql-client-12 postgresql-12 postgresql-contrib libpq-dev postgresql-server-dev-12 pgadmin3
+sudo apt -qq update
+sudo apt -qq -y install postgresql postgresql-client-12 postgresql-12 postgresql-contrib libpq-dev postgresql-server-dev-12 pgadmin3
 sudo -u postgres createuser -dlPE $(whoami)
 # Old way of Role creation:
 # sudo su - postgres
@@ -77,5 +80,5 @@ sudo -u postgres createuser -dlPE $(whoami)
 # create role myname with createdb login
 # quit
 # exit
-sudo apt install -y mlocate
+sudo apt -qq install -y mlocate
 sudo vim $(locate pg_hba.conf | grep -v sample) # if I need to config postgres, but I think I don't need because I have the role set up
